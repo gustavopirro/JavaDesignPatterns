687cf522ed69a48fc7c8811be682938492204d8c
Gustavo Gonçalves <gustavo.pirro@gmail.com>
2021-12-22 19:00:17 -0300

    config: added .gitignore

--- src/main/java/design/patterns/Main.java
+++ src/main/java/design/patterns/Main.java
@@ -2,7 +2,6 @@ package design.patterns;
 
 public class Main {
 	public static void main(String args[]) {
-		System.out.println(2+2);
 		Person gustavo = new Person("Gustavo");
 		System.out.println(gustavo.greetings());
 		
